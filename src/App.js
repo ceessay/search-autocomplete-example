01a3e292fe896f8be12ba33e04671c6f05b24b53
ceessay <mceessay@gmai.com>
2019-07-19 17:00:49 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactFetchAutocomplete from "react-fetch-autocomplete";
 
-function App() {
+// import styles from "./styles.css";
+
+// A custom suggestion parser which should always
+// return an array of objects
+// containing at least a description
+const suggestionParser = villes => {
+  return villes.map(ville => ({
+    description: ville.nom,
+    code: ville.code
+  }));
+};
+
+const App = () => {
+  const [value, setValue] = useState("");
+  const [selection, setSelection] = useState(null);
+
+
+
+  // fetchUrl is in this case a method that returns
+  // a URL but can also be a plain string
+  const fetchUrl = ({ searchQuery }) =>
+    ``;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactFetchAutocomplete
+      value={value}
+      onChange={setValue}
+      onSelect={setSelection}
+      fetchUrl={fetchUrl}
+      suggestionParser={suggestionParser}
+    >
+      {({ inputProps, getSuggestionProps, suggestions, error, loading }) => {
+        return (
+          <div>
+            <input {...inputProps({ placeholder: "Search for something.." })} />
+            {loading && <div>Loading..</div>}
+            {error && <div>We have an error..</div>}
+            {suggestions.length > 0 && (
+              <div>
+                {suggestions.map(suggestion => (
+                  <div {...getSuggestionProps(suggestion)}>
+                    <div>{suggestion.description}</div>
+                    <div>{suggestion.code}</div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </ReactFetchAutocomplete>
   );
-}
+};
 
 export default App;
