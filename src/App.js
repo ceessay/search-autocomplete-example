b9d622f70b7e50b1a309e0a39446c8c30628d240
ceessay <mceessay@gmai.com>
2019-07-19 17:32:23 +0000

    no hooks

--- src/App.js
+++ src/App.js
@@ -13,43 +13,53 @@ const suggestionParser = villes => {
   }));
 };
 
-const App = () => {
-  const [value, setValue] = useState("");
-  const [selection, setSelection] = useState(null);
+class App extends React.Component {
+  // const [value, setValue] = useState("");
+  // const [selection, setSelection] = useState(null);
+
+  state = {
+    value: "",
+    selection: null
+  };
 
   // fetchUrl is in this case a method that returns
   // a URL but can also be a plain string
-  const fetchUrl = ({ searchQuery }) => `url_se_requete_ici${searchQuery}`;
+  fetchUrl = ({ searchQuery }) =>
+    `http://51.75.205.15/backoffice/api/zones/search?q=${searchQuery}`;
 
-  return (
-    <ReactFetchAutocomplete
-      value={value}
-      onChange={setValue}
-      onSelect={setSelection}
-      fetchUrl={fetchUrl}
-      suggestionParser={suggestionParser}
-    >
-      {({ inputProps, getSuggestionProps, suggestions, error, loading }) => {
-        return (
-          <div>
-            <input {...inputProps({ placeholder: "Search for something.." })} />
-            {loading && <div>Loading..</div>}
-            {error && <div>We have an error..</div>}
-            {suggestions.length > 0 && (
-              <div>
-                {suggestions.map(suggestion => (
-                  <div {...getSuggestionProps(suggestion)}>
-                    <div>{suggestion.description}</div>
-                    <div>{suggestion.code}</div>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        );
-      }}
-    </ReactFetchAutocomplete>
-  );
-};
+  render() {
+    return (
+      <ReactFetchAutocomplete
+        value={this.state.value}
+        onChange={value => this.setState({ value })}
+        onSelect={selection => this.setState({ selection })}
+        fetchUrl={this.fetchUrl}
+        suggestionParser={suggestionParser}
+      >
+        {({ inputProps, getSuggestionProps, suggestions, error, loading }) => {
+          return (
+            <div>
+              <input
+                {...inputProps({ placeholder: "Search for something.." })}
+              />
+              {loading && <div>Loading..</div>}
+              {error && <div>We have an error..</div>}
+              {suggestions.length > 0 && (
+                <div>
+                  {suggestions.map(suggestion => (
+                    <div {...getSuggestionProps(suggestion)}>
+                      <div>{suggestion.description}</div>
+                      <div>{suggestion.code}</div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          );
+        }}
+      </ReactFetchAutocomplete>
+    );
+  }
+}
 
 export default App;
